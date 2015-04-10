26f0e20025a54b8776fcafda21aece3ccbacfdab
tristans <tristans@theodo.fr>
2015-04-10 16:06:24 +0200

    modifs

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,10 +1,10 @@
-set :application, "Tennis App"
-set :domain,      "tennis-app.com"
-set :deploy_to,   "/var/www/tennis-app.com"
+set :application, "TennisTournament"
+set :domain,      "TennisTournament.com"
+set :deploy_to,   "/var/www/TennisTournament"
+set :app_path,    "app"
 
-set :repository,  "file:///Users/deployer/sites/tennis-app"
+set :repository,  "git@github.com:tsahel/TennisTournament.git"
 set :scm,         :git
-set :deploy_via	  :copy
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
 
 set :model_manager, "doctrine"
@@ -14,9 +14,20 @@ role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain, :primary => true       # This may be the same as your `Web` server
 
 set  :keep_releases,  3
-set  :use_sudo,       false
 
-set :deploy_via, :rsync_with_remote_cache
+set :dump_assetic_assets, true
+set :use_composer, true
+
+set :shared_files,      ["app/config/parameters.yml"]
+set :shared_children,     [app_path + "/logs", web_path + "/uploads", "vendor", app_path + "/sessions"]
+
+session:
+    save_path: "%kernel.root_dir%/sessions/"
+
+set :writable_dirs,       ["app/cache", "app/logs", "app/sessions"]
+set :webserver_user,      "www-data"
+set :permission_method,   :acl
+set :use_set_permissions, true    
 
 # Be more verbose by uncommenting the following line
 # logger.level = Logger::MAX_LEVEL
